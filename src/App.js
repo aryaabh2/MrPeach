debc3001a97b9598d354986e33b573d1e3af9161
Abhishek Arya <abhi@Abhisheks-MacBook-Pro.local>
2021-05-09 20:11:04 -0400

    fixing routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SecondPage from './pages/SecondPage';
 
@@ -8,14 +8,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <BrowserRouter>
+          <HashRouter basename='/'>
             <div id="page-body">
               <Switch>
                 <Route path="/" component={HomePage} exact />
                 <Route path="/2" component={SecondPage} exact />
               </Switch>
             </div>
-          </BrowserRouter>
+          </HashRouter>
       </header>
     </div>
   );
